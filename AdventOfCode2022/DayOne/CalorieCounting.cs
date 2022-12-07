8ddcce53f73693686825376f36d12e13545b26e3
William Francis <william.francis@sats.no>
2022-12-07 14:14:17 +0100

    Advent of code setup + day one

---
+++ AdventOfCode2022/DayOne/CalorieCounting.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2022.DayOne
+{
+    static class CalorieCounting
+    {
+        public static int CountCalories()
+        {
+            var results = GetTotalCaloriesPerElf();
+
+            return results.OrderByDescending(x => x).First();
+        }
+
+        public static int CountTopThreeCalories()
+        {
+            var results = GetTotalCaloriesPerElf();
+
+            return results.OrderByDescending(x => x).Take(3).Sum();
+        }
+
+        private static List<int> GetTotalCaloriesPerElf()
+        {
+            var lines = File.ReadAllLines("Inputs/day-one-input.txt");
+
+            var results = new List<int>();
+            var calories = 0;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    results.Add(calories);
+                    calories = 0;
+                    continue;
+                }
+
+                calories += int.Parse(line);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022.DayOne;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("");
+Console.WriteLine("Lets start working our advent calendar!");
+Console.WriteLine("");
+Console.WriteLine("RESULTS:");
+Console.WriteLine("-----------------------------------------------------------------------------");
+//day one
+Console.WriteLine("Day One - Calorie Counting");
+Console.WriteLine($"Part 1  = {CalorieCounting.CountCalories()}");
+Console.WriteLine("");
+Console.WriteLine($"Part 2  = {CalorieCounting.CountTopThreeCalories()}");
+Console.WriteLine("");
+Console.WriteLine("-----------------------------------------------------------------------------");
+
+Console.ReadLine();
